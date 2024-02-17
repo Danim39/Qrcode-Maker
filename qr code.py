37a3e269bc0c210a98ed34e69a6070e248403ee6
Danim <158760301+Danim39@users.noreply.github.com>
2024-02-17 08:57:21 -0800

    Update qr code.py

--- qr code.py
+++ qr code.py
@@ -1,10 +1,8 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
 import sys
 import qrcode
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
 
 class Form(QWidget):
     def __init__(self):
